a068a5fb963f96d7b1bbe6d16cb2650ff9ec8d01
kter <me@kter.jp>
2018-06-08 23:58:48 +0900

    dynamodbから除外インスタンスを取れるように

--- hello-world-python3/lambda_function.py
+++ hello-world-python3/lambda_function.py
@@ -1,11 +1,25 @@
 # import json
 import boto3
+import os
+import json
 
 print('Loading function')
 ec2_client = boto3.client('ec2', region_name='ap-northeast-1')
 
 
 def lambda_handler(event, context):
+    if os.getenv("AWS_SAM_LOCAL"):
+        backup_exclude_list = boto3.resource(
+            'dynamodb',
+            endpoint_url="http://docker.for.mac.host.internal:8000/"
+        ).Table("backup_exclude_list")
+    else:
+        backup_exclude_list = boto3.resource('dynamodb').Table(os.getenv('TABLE_NAME'))
+    resp = backup_exclude_list.scan()
+
+    # print json.dumps({item['instanceid']: int(item['votes']) for item in resp['Items']})
+    print(resp['Items'])
+
     response = ec2_client.describe_instances()
     for ec2_group in response['Reservations']:
         for instance_info in ec2_group['Instances']:
