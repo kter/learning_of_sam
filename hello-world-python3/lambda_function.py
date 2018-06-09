a7ddfd3b06392cc7dc1c0c9352f2a7caf206d97f
kter <me@kter.jp>
2018-06-09 12:14:33 +0900

    refactoring

--- hello-world-python3/lambda_function.py
+++ hello-world-python3/lambda_function.py
@@ -1,10 +1,8 @@
 # import json
 import boto3
 import os
-import json
 
 print('Loading function')
-ec2_client = boto3.client('ec2', region_name='ap-northeast-1')
 
 
 def lambda_handler(event, context):
@@ -14,16 +12,20 @@ def lambda_handler(event, context):
             endpoint_url="http://docker.for.mac.host.internal:8000/"
         ).Table("backup_exclude_list")
     else:
-        backup_exclude_list = boto3.resource('dynamodb').Table(os.getenv('TABLE_NAME'))
+        backup_exclude_list = \
+            boto3.resource('dynamodb').Table(os.getenv('TABLE_NAME'))
     resp = backup_exclude_list.scan()['Items']
 
-    # print json.dumps({item['instanceid']: int(item['votes']) for item in resp['Items']})
-    for exclude_instance in resp:
-        print(exclude_instance)
+    print("exclude instances")
+    for exclude in resp:
+        print(exclude['instanceid'])
 
+    print("instances")
+    # TODO classified
+    ec2_client = boto3.client('ec2', region_name='ap-northeast-1')
     response = ec2_client.describe_instances()
     for ec2_group in response['Reservations']:
-        for instance_info in ec2_group['Instances']:
-            print(instance_info['InstanceId'])
+        for instance in ec2_group['Instances']:
+            print(instance['InstanceId'])
 
     return True
